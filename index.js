254a9a1ffd8c9c133e82bae58d73deec770166b2
Daniel Hudson <LuckyLlama72@gmail.com>
2024-06-11 14:30:54 -0400

    Added input for questions

--- index.js
+++ index.js
@@ -1,13 +1,45 @@
 // TODO: Include packages needed for this application
-
+const fs = require("fs");
+const inquirer = require("inquirer");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title?",
+    name: "Title",
+    }, {
+        type: "input",
+        message: "How is it installed? ",
+        name: "Installation",
+        }, {
+            type: "input",
+            message: "How is it used? ",
+            name: "Usage",
+            }, {
+                type: "input",
+                message: "How can others contribute? ",
+                name: "Contributing",
+                }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    console.log("Welcome to README Generator V2!")
+    inquirer
+        .prompt(questions)
+        .then((response) =>
+        console.log(response))
+    }
+
+    // inquirer
+    //     .prompt({
+    //         tpye: "input",
+    //         message: "What is the title of this README? ",
+    //         name: "Title",
+    //     })
+    //     .then((response) => 
+    //     console.log(response))
 
 // Function call to initialize app
 init();
